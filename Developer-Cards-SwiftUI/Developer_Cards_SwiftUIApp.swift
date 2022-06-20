7ef7bdc1c6c799d9ecb80c8dba7a10b7669c88e9
hakkicansengonul <coder.9734@gmail.com>
2022-06-20 15:30:16 +0300

    Initial Commit

---
+++ Developer-Cards-SwiftUI/Developer_Cards_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Developer_Cards_SwiftUIApp.swift
+//  Developer-Cards-SwiftUI
+//
+//  Created by hakkı can şengönül on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Developer_Cards_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
