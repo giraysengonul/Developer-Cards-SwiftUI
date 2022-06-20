d1ed8c9c566ee1c4a2e1de54e270226a5a92d748
hakkicansengonul <coder.9734@gmail.com>
2022-06-20 17:32:14 +0300

    Added alert and haptic feedback

--- Developer-Cards-SwiftUI/View/CardView.swift
+++ Developer-Cards-SwiftUI/View/CardView.swift
@@ -12,6 +12,8 @@ struct CardView: View {
     @State private var fadeIn : Bool = false
     @State private var moveDownward : Bool = false
     @State private var moveUpward : Bool = false
+    @State private var showAlert : Bool = false
+    var hapticImpact = UIImpactFeedbackGenerator(style: .heavy)
     var card : Card
     // MARK: - BODY
     var body: some View {
@@ -36,7 +38,9 @@ struct CardView: View {
                     
                 )
             Button {
+                hapticImpact.impactOccurred()
                 playSound(sound: "sound-chime", type: "mp3")
+                showAlert.toggle()
             } label: {
                 HStack {
                     Text(card.callToAction.uppercased())
@@ -70,6 +74,9 @@ struct CardView: View {
                 self.moveDownward.toggle()
                 self.moveUpward.toggle()
             }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text(card.title), message: Text(card.message), dismissButton:.default(Text("Ok")))
         }
     }
 }
